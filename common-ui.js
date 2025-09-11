45279aabaf106306fae40da92d2949a03951ab5f
IrwanS <32952195+Milkywan@users.noreply.github.com>
2025-09-11 17:02:01 +0900

    Update common-ui.js

--- common-ui.js
+++ common-ui.js
@@ -148,13 +148,7 @@ function renderHomeMenuItems(userRole, mainMenuGridId) {
         mainMenuGrid.appendChild(superAdminMenuSettings);
     }
 
-    // Tambahkan tombol Logout sebagai elemen terakhir
-    const logoutMenuCard = document.createElement("div");
-    logoutMenuCard.className = "menu-card";
-    logoutMenuCard.onclick = () => window.logout();
-    logoutMenuCard.textContent = "🚪 Logout";
-    mainMenuGrid.appendChild(logoutMenuCard);
-}
+  
 
 
 // --- MAIN INITIALIZER FUNCTION ---
@@ -224,3 +218,4 @@ export async function initPage(pageTitle, mainContentId, requiredRole, homeMenuG
         }
     });
 }
+
